9cb656a09fc7ec2acb55b54afb3434dabcdea29e
caoxh <caoxh@csdn.net>
2012-10-12 11:34:28 +0800

    修改vundle安装方式

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 ln -s `pwd` ~/.vim
 ln -s `pwd`/vimrc ~/.vimrc
-git submodule init
-git submodule update
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim +BundleInstall +qall #install vim plugin
